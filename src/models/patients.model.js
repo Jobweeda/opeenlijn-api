f099cc817fc50d58dc63d8f806416e75a81ecd7f
jobweeda <jobweeda@hotmail.com>
2017-10-31 17:26:59 +0100

    adds patientmodel to api

---
+++ src/models/patients.model.js
@@ -0,0 +1,19 @@
+// patients-model.js - A mongoose model
+//
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+module.exports = function (app) {
+  const mongooseClient = app.get('mongooseClient');
+  const { Schema } = mongooseClient;
+  const patients = new Schema({
+
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String },
+    appointments: { type: [Date] },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
+  });
+
+  return mongooseClient.model('patients', patients);
+};
--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,5 @@
+const patients = require('./patients/patients.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
+  app.configure(patients);
 };
